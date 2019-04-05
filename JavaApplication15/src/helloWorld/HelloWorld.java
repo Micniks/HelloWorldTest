31e9c088947f1999ba0bf3f9c7117a5cfedcc7f7
Bruger <Bruger@10.50.137.232>
2019-04-05 09:42:50 +0200

    Added some random stuff

--- JavaApplication15/src/helloWorld/HelloWorld.java
+++ JavaApplication15/src/helloWorld/HelloWorld.java
@@ -8,6 +8,11 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("this is test 1:");
+        int a, b;
+        a = 5;
+        b = 7;
+        System.out.println(a*b);
         
     }
     
